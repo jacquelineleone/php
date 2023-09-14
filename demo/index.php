b31ca8450e89e4ba06581aa54387e1a42a5ac934
Jacqueline <leonej.grupodelsud@gmail.com>
2023-09-14 11:52:43 -0300

    first commit

---
+++ demo/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <title>Demo</title>
+</head>
+
+<body>
+  <h2>
+    <?php
+    echo "Hello World";
+    ?>
+  </h2>
+</body>
+
+</html>
